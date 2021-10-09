4bb8e858a73851f21399fc67d03a2191a14dacb1
Marcos Paulo M Cabral <69735297+MarcosCabral93@users.noreply.github.com>
2021-10-09 18:09:18 -0300

    Criando jogo da forca e adivinhacao

---
+++ Trabalhando_com_condicionais.py
@@ -0,0 +1,9 @@
+print('Programa exemplo de condicionais')
+print(20*'#')
+
+idade= int(input("Digite sua Idade "))
+if idade >= 18:
+    print('voce é maior de idad')
+else:
+    print("Voce é menor de idad")
+
---
+++ Trabalhando_com_for.py
@@ -0,0 +1,14 @@
+
+
+nome ="Marcos Paulo"
+lista=[1,2,2,3,3]
+numeros =range (1,10)
+#for letra in nome:
+ #   print(letra, end='')
+
+#for n in range(1,11):
+   # print(n)
+#for n in range(0,1001,100):
+  #  print(n)
+#for n in range(10, -1,-1):
+  #  print(n)
